9ea876089b55b8d6b080cd12719060ca6c8b3ce0
wascode93 <waseem.m.salama@gmail.com>
2021-02-22 14:44:46 +0200

    - renamed EmailScheduler to EmailNotificationScheduler

--- src/main/java/com/swvl/challenge/notification/schedulers/EmailNotificationScheduler.java
+++ src/main/java/com/swvl/challenge/notification/schedulers/EmailNotificationScheduler.java
@@ -2,7 +2,7 @@ package com.swvl.challenge.notification.schedulers;
 
 import com.swvl.challenge.notification.models.EmailNotification;
 import com.swvl.challenge.notification.models.User;
-import com.swvl.challenge.notification.providers.EmailSendStrategy;
+import com.swvl.challenge.notification.providers.EmailNotificationSendStrategy;
 import com.swvl.challenge.notification.services.EmailNotificationService;
 import com.swvl.challenge.notification.services.UserService;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,19 +11,19 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class EmailScheduler {
+public class EmailNotificationScheduler {
 
-  private final int EMAIL_PROVIDER_LIMIT_PER_MINUTE = 30;
+  private final int PROVIDER_LIMIT_PER_MINUTE = 30;
   private final EmailNotificationService emailNotificationService;
-  private final EmailSendStrategy emailSendStrategy;
+  private final EmailNotificationSendStrategy emailNotificationSendStrategy;
   private final UserService userService;
 
-  public EmailScheduler(
-      EmailNotificationService emailNotificationService,
-      EmailSendStrategy emailSendStrategy,
-      UserService userService) {
+  public EmailNotificationScheduler(
+          EmailNotificationService emailNotificationService,
+          EmailNotificationSendStrategy emailNotificationSendStrategy,
+          UserService userService) {
     this.emailNotificationService = emailNotificationService;
-    this.emailSendStrategy = emailSendStrategy;
+    this.emailNotificationSendStrategy = emailNotificationSendStrategy;
     this.userService = userService;
   }
 
@@ -36,13 +36,13 @@ public class EmailScheduler {
 
   private List<EmailNotification> getNotifications() {
     return emailNotificationService.getAllPendingNotificationsByLimit(
-        EMAIL_PROVIDER_LIMIT_PER_MINUTE);
+            PROVIDER_LIMIT_PER_MINUTE);
   }
 
   private void sendNotifications(List<EmailNotification> notifications) {
     for (EmailNotification notification : notifications) {
       User user = userService.findUserById(notification.getUserId());
-      emailSendStrategy.send(user.getEmail(), notification.getMessage());
+      emailNotificationSendStrategy.send(user.getEmail(), notification.getMessage());
       notification.setSent(true);
     }
   }
