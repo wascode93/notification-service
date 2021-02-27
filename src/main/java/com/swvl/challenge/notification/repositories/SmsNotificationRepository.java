22c94bccdd75039f6be542b2b7d71d6617ea3bfe
wascode93 <waseem.m.salama@gmail.com>
2021-02-27 15:32:03 +0200

    - modified SmsNotificationRepository to extend NotificationRepository

--- src/main/java/com/swvl/challenge/notification/repositories/SmsNotificationRepository.java
+++ src/main/java/com/swvl/challenge/notification/repositories/SmsNotificationRepository.java
@@ -1,11 +1,5 @@
 package com.swvl.challenge.notification.repositories;
 
 import com.swvl.challenge.notification.models.SmsNotification;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface SmsNotificationRepository extends JpaRepository<SmsNotification, Long> {
-  List<SmsNotification> findAllBySentIsFalse(Pageable page);
-}
+public interface SmsNotificationRepository extends NotificationRepository<SmsNotification> {}
