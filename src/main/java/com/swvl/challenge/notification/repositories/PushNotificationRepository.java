b5c30aa2587bcce2b624b38945351574d774556b
wascode93 <waseem.m.salama@gmail.com>
2021-02-27 15:31:55 +0200

    - modified PushNotificationRepository to extend NotificationRepository

--- src/main/java/com/swvl/challenge/notification/repositories/PushNotificationRepository.java
+++ src/main/java/com/swvl/challenge/notification/repositories/PushNotificationRepository.java
@@ -2,11 +2,9 @@ package com.swvl.challenge.notification.repositories;
 
 import com.swvl.challenge.notification.models.PushNotification;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PushNotificationRepository extends JpaRepository<PushNotification, Long> {
-  List<PushNotification> findAllBySentIsFalse(Pageable page);
+public interface PushNotificationRepository extends NotificationRepository<PushNotification> {
   List<PushNotification> findAllByUserIdAndSentIsTrue(Long userId, Pageable page);
 }
